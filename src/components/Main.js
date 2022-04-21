b2c7bfc8dd963333aa82d467aef544c84d726b1b
unknown <royrozay99@gmail.com>
2022-04-21 22:57:19 +0100

    new additions

--- src/components/Main.js
+++ src/components/Main.js
@@ -52,6 +52,50 @@ function Main() {
                     </div>
                 </div>
             </div>
+
+            <div className={styles.container}>
+                <h2>
+                    5 ways to build your savings
+                </h2>
+                <div className={styles.grid}>
+                    <div className={styles.piggy}>
+                        <img src={Images.piggybank_icon} alt="piggybank_icon" />
+                        <div className={styles.savings}>
+                            <span>AUTOMATED SAVINGS</span><br />
+                            <p>Build a dedicated savings faster on your terms automatically or manually.</p>
+                        </div>
+                    </div>
+                    <div className={styles.piggy}>
+                        <img src={Images.safelock_icon} alt="safelock_icon" />
+                        <div className={styles.savings}>
+                            <span>FIXED SAVINGS</span><br />
+                            <p>Lock money away for a fixed duration with no access to it until maturity. It’s like having a custom fixed deposit</p>
+                        </div>
+                    </div>
+                    <div className={styles.piggy}>
+                        <img src={Images.Greendot} alt="Greendot" />
+                        <div className={styles.savings}>
+                            <span>GOAL-ORIENTED SAVINGS</span><br />
+                            <p>Reach all your savings goals faster. Save towards multiple goals on your own or with a group.</p>
+                        </div>
+                    </div>
+                    <div className={styles.piggy}>
+                        <img src={Images.naira_icon} alt="naira_icon" />
+                        <div className={styles.savings}>
+                            <span>FLEXIBLE SAVINGS</span><br />
+                            <p>Save, transfer, withdraw, manage and organise your money for free at any time</p>
+                        </div>
+                    </div>
+                    <div className={styles.piggy}>
+                        <img src={Images.dollar_icon} alt="dollar_icon" />
+                        <div className={styles.savings}>
+                            <span>DOLLAR SAVINGS</span><br />
+                            <p>Save in foreign currencies such as USD and Euros.</p>
+                        </div>
+                    </div>
+                    <img src={Images.save_phone_min} className={styles.save_phone_min} alt="save_phone_min" />
+                </div>
+            </div>
         </main>
     );
 }
--- src/img/Images.js
+++ src/img/Images.js
@@ -7,8 +7,14 @@ import Google_play from "./Google_play.svg";
 import kola from "./kola.svg";
 import key_icon from "./key_icon.svg";
 import Preloader_icon from "./Preloader_icon.svg";
+import piggybank_icon from "./piggybank_icon.svg";
+import safelock_icon from "./safelock_icon.svg";
+import Greendot from "./Greendot.svg";
+import naira_icon from "./naira_icon.svg";
+import dollar_icon from "./dollar_icon.svg";
+import save_phone_min from "./save_phone_min.svg";
 
 const Images = {
-    PiggyVest,Justin,Aisha,Angela,App_Store,Google_play,kola,key_icon,Preloader_icon
+    PiggyVest,Justin,Aisha,Angela,App_Store,Google_play,kola,key_icon,Preloader_icon,piggybank_icon,safelock_icon,Greendot,naira_icon,dollar_icon,save_phone_min
 }
 export default Images;
