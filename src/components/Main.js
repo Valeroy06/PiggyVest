f02b4082c916fab2f581cfda4fc117581bff16e1
unknown <royrozay99@gmail.com>
2022-04-21 09:39:07 +0100

    new additions

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import Images from "../img/Images";
-import styles from "../styles/modules/main.module.scss"
+import styles from "../styles/modules/main.module.scss";
 
 function Main() {
     return (
@@ -7,24 +7,51 @@ function Main() {
             <div className={styles.best}>
                 <img src={Images.Justin} className={styles.Justin} alt="Justin" />
                 <div className={styles.save}>
-                    <h1>The Best way <br/>
-                        to Save & Invest</h1>
-                    <p>PiggyVest helps over 1 million customers achieve their financial goals by helping them save and invest with ease.</p>
+                    <h1>
+                        The Best way <br />
+                        to Save & Invest
+                    </h1>
+                    <p>
+                        PiggyVest helps over 1 million customers achieve their financial
+                        goals by helping them save and invest with ease.
+                    </p>
                     <button>Create A Free Account</button>
                 </div>
                 <img src={Images.Aisha} className={styles.Aisha} alt="Aisha" />
             </div>
             <div className={styles.imgs}>
                 <img src={Images.Angela} alt="Angela" />
-                <div>
-                    <img src={Images.App_Store} alt="App_Store" />
+                <div className={styles.stores}>
+                    <img
+                        src={Images.App_Store}
+                        className={styles.App_Store}
+                        alt="App_Store"
+                    />
                     <img src={Images.Google_play} alt="Google_play" />
                 </div>
                 <img src={Images.kola} alt="kola" />
-
             </div>
-
-
+            <div>
+                <h2>Security Comes First</h2>
+                <div className={styles.content}>
+                    <div className={styles.key}>
+                        <img src={Images.key_icon} alt="key_icon" />
+                        <p>
+                            PiggyVest uses the highest levels of Internet Security, and it is
+                            secured by 256 bits SSL security encryption to ensure that your
+                            information is completely protected from fraud.
+                        </p>
+                    </div>
+                    <div className={styles.preloader}>
+                        <img src={Images.Preloader_icon} alt="Preloader_icon" />
+                        <p>
+                            Two-factor authentication (2FA) by default on all PiggyVest
+                            accounts, to securely protect you from unauthorised access and
+                            impersonation.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </main>
     );
 }
--- src/img/Images.js
+++ src/img/Images.js
@@ -5,8 +5,10 @@ import Angela from "./Angela.svg";
 import App_Store from "./App_Store.svg";
 import Google_play from "./Google_play.svg";
 import kola from "./kola.svg";
+import key_icon from "./key_icon.svg";
+import Preloader_icon from "./Preloader_icon.svg";
 
 const Images = {
-    PiggyVest,Justin,Aisha,Angela,App_Store,Google_play,kola
+    PiggyVest,Justin,Aisha,Angela,App_Store,Google_play,kola,key_icon,Preloader_icon
 }
 export default Images;
